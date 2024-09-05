a81610e012769e49436f55a04f2d4036ee393323
Pankaj kumar <pankaj14badgujar@gmail.com>
2024-09-05 21:47:06 +0530

    5/9/24 add static in shop

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="Shophome")
+    path("", views.index, name="Shophome"),
+    path("about/", views.about, name="aboutus"),
+    path("contact/", views.contact, name="contactus"),
+    path("search/", views.search, name="search"),
+    path("productView/", views.productView, name="productView"),
+    path("checkout/", views.checkout, name="checkout"),    
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Shop home page......")
+    #return HttpResponse("Shop home page......")
+    return render(request,"shop/index.html")
+
+def about(request):
+    return HttpResponse("This is aboutUs Page")
+
+def contact(request):
+    return HttpResponse("This is contact Page")
+
+def search(request):
+    return HttpResponse("This is search Page")
+
+def productView(request):
+    return HttpResponse("This is productView Page")
+
+def checkout(request):
+    return HttpResponse("This is checkout Page")
+
+
